f9620c25319abe760fee6c6aef6330132fc22018
lsj8687 <slieu3@gatech.edu>
2022-07-24 15:10:04 -0400

    0724

--- js/accessibility.js
+++ js/accessibility.js
@@ -15,7 +15,28 @@ const mapAftAcc = new mapboxgl.Map({
     minZoom: 3,
 });
 
+// default (Absolute measure) - only the slider_abs shown
+document.getElementsByClassName("sliderAccContentRelative")[0].style.visibility = "hidden";
 
+// show the absolute slider while hiding the relative one - by clicking the radio button
+function getSliderAcc_abs(){
+    document.getElementsByClassName("sliderAccContentAbsolute")[0].style.visibility = "visible";
+    document.getElementsByClassName("sliderAccContentAbsolute")[0].style.zIndex = "1";
+    document.getElementsByClassName("sliderAccContentRelative")[0].style.visibility = "hidden";
+    document.getElementById("myRangeAcc_abs").value = 45;
+    timeAcc_filter = 45;
+    sliderValueAcc_abs.innerHTML = 45;
+}
+// show the relative slider while hiding the absolute one - by clicking the radio button
+function getSliderAcc_rel(){
+    document.getElementsByClassName("sliderAccContentAbsolute")[0].style.visibility = "hidden";
+    document.getElementsByClassName("sliderAccContentRelative")[0].style.visibility = "visible";
+    document.getElementsByClassName("sliderAccContentRelative")[0].style.zIndex = "1";
+    document.getElementById("myRangeAcc_rel").value = 2;
+    // reset the value and slidervalue
+    timeAcc_filter = 2;
+    sliderValueAcc_rel.innerHTML = 2;
+}
 
 
 function coordinateAcc() {
